ada8570061b60fd34ed39f20a2669e9ff5ed9c65
MohamadNosrati <mohamadnosratidev@gmail.com>
2026-06-15 16:00:03 +0330

    posthug added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,21 @@
+import CustomContainer from "@/components/ui/CustomContainer";
+import { Button } from "@heroui/button";
+import Link from "next/link";
+
 const Dashboard = () => {
-  return <section />;
+  return (
+    <CustomContainer className="flex justify-between">
+      <p className="text-white font-bold text-2xl">
+        برای مشاهده متریک های مختلف سابت (web analytics) به لینک روبرو مراجع
+        کنید.
+      </p>
+      <Link href={process.env.NEXT_PUBLIC_POSTHOG_ANALYTICS_LINK as string}>
+        <Button className="text-white" size="lg" color="success">
+          posthug
+        </Button>
+      </Link>
+    </CustomContainer>
+  );
 };
 
 export default Dashboard;
